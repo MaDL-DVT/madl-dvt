e86be531eb0b32d323110234ba4e4bf5ecf87177
Alexander Fedotov <a.fedotov@tue.nl>
2019-11-12 16:17:46 +0100

    gitignore and dac_experiments patches

--- examples/dac_experiments.py
+++ examples/dac_experiments.py
@@ -1,10 +1,11 @@
 import os
 import subprocess
 import time
+import errno
 
-dir = os.getcwd()
-gng_dir = os.path.join(dir,'go_no_go')
-pc_dir = os.path.join(dir,'power_clock')
+curdir = os.getcwd()
+gng_dir = os.path.join(curdir,'go_no_go')
+pc_dir = os.path.join(curdir,'power_clock')
 
 gng_name = 'go_no_go_top'
 gng_suffixes = ['1','1_dl','3','3_dl','7','7_dl','11','11_dl','15','15_dl','23','23_dl']
@@ -14,20 +15,34 @@ pc_suffixes = ['1_2','1_2_dl','1_3','1_3_dl','2_2','2_2_dl','3_2','3_2_dl']
 
 results_name = "results.txt"
 
+def check_dlockdetect_exists():
+    # Call raises an exception if command falls
+    subprocess.check_call(['dlockdetect', '--help'],
+                          stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+
 def run_test(tname,suffixes,res_file):
     for i in suffixes:
         f = tname + '_' + i + '.madl'
-        name = os.path.join(dir,'go_no_go',f)
+        name = os.path.join(curdir,'go_no_go',f)
+        print("Testing {0} using SMT solver".format(name))
         start_time = time.time()
         gng_res = subprocess.run(['time','dlockdetect','-f',name,'--no-cyclecheck','--simultaneous-smt','--smt-only'], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+        if gng_res.returncode < 0:
+            print("Case {0}, SMT failed with exit code {1}".format(name, gng_res.returncode))
         res_file.write(f + '\t\t\t' + 'smt' + '\t' + str(time.time()-start_time) + '\n')
         start_time = time.time()
+        print("Testing {0} using nuXMV (reachability)".format(name))
         gng_res = subprocess.run(['time','dlockdetect','-f',name,'--no-cyclecheck','--simultaneous-smt','--reachability-only'], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+        if gng_res.returncode < 0:
+            print("Case {0}, reachability failed with exit code {1}".format(
+                name, gng_res.returncode))
         res_file.write(f + '\t\t\t' + 'smv' + '\t' + str(time.time()-start_time) + '\n')
 
-file = open(results_name,'w+')
+check_dlockdetect_exists()
 
-run_test(gng_name,gng_suffixes,file)
-run_test(pc_name,pc_suffixes,file)
+resfile = open(results_name,'w+')
 
-file.close()
+run_test(gng_name,gng_suffixes,resfile)
+run_test(pc_name,pc_suffixes,resfile)
+
+resfile.close()
